57dd8c5cab48fa55a649da3ded20b67ae0ba0bd5
Rodryvaldivia <rodryvaldivia22@gmail.com>
2021-01-14 11:58:24 -0300

    Ejercicio 1

--- EJ1.js
+++ EJ1.js
@@ -4,6 +4,7 @@
 3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
 4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
 */
+console.log("1)");
 
 { const str = "Uno dos tres";
 
@@ -18,6 +19,8 @@ try {
 }
 
 }
+console.log("2)");
+
 {
 const recortador = function(str,indice) {
     console.log(`${str}`.substring(null,`${indice}`))
@@ -26,4 +29,19 @@ const recortador = function(str,indice) {
 const str = "Hola Mundo",
 indice = 4;
 recortador(str,indice);
+}
+console.log("3)");
+
+{
+    const separador = function(str) {
+        console.log(`${str}`.split(" "));
+    }
+    separador("Hola que tal"," ");
+}
+console.log("4)");
+{
+    const repetidor = function(lista,numRep) {
+        console.log(`${lista}`.repeat(`${numRep}`));
+    }
+    repetidor("Hola Mundo ",3);
 }
