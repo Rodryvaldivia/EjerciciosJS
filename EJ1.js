e6baa39020c719f2a5aa7385e2e9818ad943a2b3
Rodryvaldivia <rodryvaldivia22@gmail.com>
2021-01-14 10:12:46 -0300

    ejercicios 1

---
+++ EJ1.js
@@ -0,0 +1,18 @@
+/*
+1) Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
+2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
+3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
+4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
+*/
+
+const str = "Uno dos tres";
+
+try {
+    if(typeof str === "string") {
+        console.log(str.length)
+    } else {
+        throw new Error(`"${str}" No es un string`)
+    }
+} catch (error) {
+    console.log(`Se ha encontrado un ${error}`);
+}
